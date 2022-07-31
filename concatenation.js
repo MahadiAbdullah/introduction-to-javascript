7024093a1f4a11671f636c015ec1899aa36095a4
MahadiAbdullah <mahadi8603@gmail.com>
2022-07-31 18:14:36 +0600

    new practice added

--- concatenation.js
+++ concatenation.js
@@ -1,4 +1,38 @@
 var firstName = 'jannie';
 var lastName ='Kim';
 var fullName = firstName + ' ' + lastName;
-console.log(fullName);
+// console.log(fullName);
+
+// Practice Problem1
+ var giveMoney = 1000;
+ var oranges = 400;
+ var apple = 300;
+ var cost = oranges + apple;
+//  console.log(cost);
+ var returnMoney = giveMoney - cost;
+//  console.log(returnMoney);
+//  Practice Problem2
+var mathematics = 75.25;
+var bioligy = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+var total = mathematics + bioligy + chemistry + physics + bangla;
+// console.log(total);
+var avarage = total / 5;
+// console.log(avarage);
+var avarage = avarage.toFixed(2);
+avarage = parseFloat(avarage);
+// console.log(avarage);
+// Practice Problem3
+
+var firstLine = "I am going to be";
+var secondLine = "an awesome web developer";
+var total = firstLine + ' ' + secondLine;
+// console.log(total);
+
+// Practice Problem4
+var number1 = 119;
+var number2 = 5;
+var total = number1 % number2;
+console.log(total);
